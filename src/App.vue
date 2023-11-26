a0961228cfe70f2b69f27afc608b3dea78b9bbd6
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-11-26 13:51:07 +0100

    Feat: setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
+import Scores from './components/Scores.vue';
 
 </script>
 
 <template>
-
+        <h1>Scoreboard</h1>
+        <div class="scoreboard">
+            <Scores teamName="team1" id="team1"/>
+            <Scores teamName="team2" id="team2"/>
+        </div>
+    
 </template>
 
 <style scoped>
-
+.scoreboard{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 1rem;
+    box-shadow: 0 0 1rem rgba(0,0,0,0.2);
+}
 </style>
---
+++ src/components/Scores.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps, ref } from 'vue';
+const props = defineProps(['teamName', 'socket']);
+const score = ref(0);
+</script>
+
+<template>
+    <div class="scores">
+        <h2>{{ teamName }}</h2>
+        <p>Sporting Wolvertem</p>
+        <div class="score">
+            0
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.score{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    font-size: 10rem;
+    font-weight: bold;
+    
+}
+</style>
